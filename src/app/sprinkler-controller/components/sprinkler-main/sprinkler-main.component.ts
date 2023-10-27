45a1cfd1876016edb3ec67f2bba7a177cc37331b
priyanka <priyanka.kannan@solitontech.com>
2023-10-27 10:51:12 +0530

    feat: sprinker main: adds power button
    Signed-off-by: priyanka <priyanka.kannan@solitontech.com>

--- src/app/sprinkler-controller/components/sprinkler-main/sprinkler-main.component.ts
+++ src/app/sprinkler-controller/components/sprinkler-main/sprinkler-main.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sprinkler-main',
   templateUrl: './sprinkler-main.component.html',
-  styleUrls: ['./sprinkler-main.component.scss']
+  styleUrls: ['./sprinkler-main.component.scss'],
 })
 export class SprinklerMainComponent {
+  isChecked: boolean = false;
 
+  toStartApplication() {
+  }
 }
